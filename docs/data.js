ba05f9111bcbc5acacdb647b2d5e0c99dca5cc04
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-01-05 12:28:51 -0600

    Update and rename reports.json to data.js

--- docs/data.js
+++ docs/data.js
@@ -1,4 +1,4 @@
- [
+ export const jsonData = [
   { "id": 1, "name": "Report 1", url:"/testMultipleReports/html-report-1/index.html" },
   { "id": 2, "name": "Report 2", url:"/testMultipleReports/html-report-2/index.html" },
-]
+];
