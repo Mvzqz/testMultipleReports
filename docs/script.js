5ebf52dac2946ecdd0b587bfe9c75a7d61a72483
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-01-05 12:21:25 -0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 var jsonData = [
-  { "id": 1, "name": "Report 1", url:"html-report-1/index.html" },
-  { "id": 2, "name": "Report 2", url:"html-report-2/index.html" },
+  { "id": 1, "name": "Report 1", url:"/testMultipleReports/html-report-1/index.html" },
+  { "id": 2, "name": "Report 2", url:"/testMultipleReports/html-report-2/index.html" },
 ];
 
 console.log("test log")
