7f6e8e3b25ce7cf28a14fb073b9d684b580ce308
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-01-05 12:11:46 -0600

    Update and rename create-list.js to script.js

---
+++ docs/script.js
@@ -0,0 +1,31 @@
+var jsonData = [
+  { "id": 1, "name": "Report 1", url:"testMultipleReports/html-report-1/index.html" },
+  { "id": 2, "name": "Report 2", url:"testMultipleReports/html-report-2/index.html" },
+];
+
+console.log("test log")
+
+
+// Create an <ul> element
+var ulElement = document.createElement('ul');
+
+// Loop through the JSON data
+jsonData.forEach(function(item) {
+  // Create a <li> element for each item
+  var liElement = document.createElement('li');
+  var aElement = document.createElement('a');
+
+  // Set the text content of the <li> element
+  aElement.textContent = item.name
+  aElement.href = item.url
+  liElement.appendChild(aElement);
+
+  // Append the <li> element to the <ul> element
+  ulElement.appendChild(liElement);
+});
+
+// Assuming you have a <ul> element in your HTML with an id="list-container"
+var listContainer = document.getElementById('list-container');
+
+// Append the <ul> element to the container in the HTML
+listContainer.appendChild(ulElement);
