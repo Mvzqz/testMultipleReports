6b019cd220beebc5d713b1bc86279cdf3a6452a8
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-01-05 12:28:12 -0600

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,4 @@
-var jsonData = [
-  { "id": 1, "name": "Report 1", url:"/testMultipleReports/html-report-1/index.html" },
-  { "id": 2, "name": "Report 2", url:"/testMultipleReports/html-report-2/index.html" },
-];
+import { jsonData } from './data.js';
 
 console.log("test log")
 
