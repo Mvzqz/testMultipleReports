0cd430c65320a451cb3c17099886db9b3d1a9410
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-02-12 17:39:07 -0600

    Update test1.spec.ts

--- test/test1.spec.ts
+++ test/test1.spec.ts
@@ -2,14 +2,6 @@ import {test, expect,} from '@playwright/test'
 
 for (let i = 0; i < 10; i++) {
   test(`test ${i}`, async({ page }) => {
-        test.slow()
-        await page.goto('https://www.whatismyip.com')
-
-        const ipElements = await page.locator('.ip-info-line').all()
-        for(const ipElement of ipElements){
-            console.log(await ipElement.textContent())
-        }
-
-        await expect(page.locator('#geo')).toContainText('US')
+      console.log("TEST")
     })
 }
