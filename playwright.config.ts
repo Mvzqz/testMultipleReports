3864fb6f7b691afec441247636ee99c23ac2f9dc
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-02-12 17:52:03 -0600

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@playwright/test";
 
 
 export default defineConfig({
-    testDir:'./tests',
+    testDir:'./test',
     reporter:[
         ['blob'],
         ['line'],
