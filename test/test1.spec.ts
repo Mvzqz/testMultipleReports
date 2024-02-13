b4c9869af2d17fc5c4677a2ebd51d98f46ebbc07
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-02-12 18:54:30 -0600

    Update test1.spec.ts

--- test/test1.spec.ts
+++ test/test1.spec.ts
@@ -1,8 +1,6 @@
 import {test, expect} from '@playwright/test'
-
-for (let i = 0; i < 10; i++) {
+  const i= 1
   test(`test ${i}`, async({ page }) => {
       console.log(`test ${i}`)
       expect(i%2).toBe(0)
     })
-}
