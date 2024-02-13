72c6374801bd6a79fef5d30648df3836e62b245c
Mvzqz <pon.tu.correo.aca@gmail.com>
2024-02-12 18:55:57 -0600

    Update test1.spec.ts

--- test/test1.spec.ts
+++ test/test1.spec.ts
@@ -1,5 +1,50 @@
 import {test, expect} from '@playwright/test'
-  const i= 1
+  let i= 1
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=2
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=3
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=4
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=5
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=6
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=7
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=8
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=9
+  test(`test ${i}`, async({ page }) => {
+      console.log(`test ${i}`)
+      expect(i%2).toBe(0)
+    })
+  i=10
   test(`test ${i}`, async({ page }) => {
       console.log(`test ${i}`)
       expect(i%2).toBe(0)
